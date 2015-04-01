ff217465b5347c6e84069219ad2c1d4317997dbc
hoenirvili <hoenirvili@gmail.com>
2015-04-01 17:21:21 +0300

    fixed nav errors

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt){
 	grunt.initConfig({
-		
+
 		pkg:grunt.file.readJSON('package.json'),
-			// concat 
+			// concat
 			concat:{
 				css:{
 					src:['css/index.css','css/login.css','register.css'],
 					dest: 'css/main.css',
 				},
-				
+
 				js:{
 					src:['js/index.js'],
 					dest:'js/main.js',
@@ -16,7 +16,7 @@ module.exports = function(grunt){
 			},
 		watch:{
 			css: {
-				files: ['css/*.css'],				
+				files: ['css/*.css'],
 				tasks: ['concat'],
 			},
 			js: {
@@ -28,7 +28,7 @@ module.exports = function(grunt){
 			}
 		},
 		express:{
-			
+
 			all:{
 					options:{
 					port: 9000, // server at port 9000
@@ -38,7 +38,7 @@ module.exports = function(grunt){
 				}
 			}
 		}
-	}); 
+	});
 		grunt.loadNpmTasks('grunt-contrib-watch');
 		grunt.loadNpmTasks('grunt-contrib-concat');
 		grunt.loadNpmTasks('grunt-express');
---
+++ php/login.php
@@ -0,0 +1,12 @@
+<?php
+
+    class login
+    {
+      public functon __construct
+      {
+
+      }
+    }
+
+
+?>

74ce7ad658d459e7e53fa156ee7bdc0423f9b523
hoenirvili <hoenirvili@gmail.com>
2015-04-10 23:26:48 +0300

    trying to figure out how to animate the 3 div of section one

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 $(window).load(function(){
 	$(".util-pie").fadeOut("slow");
 });
+
+// when scrolling 
+
+var elementOne = document.getElementById("el1");
+var elementTwo = document.getElementById("el2");
+var elementThree = document.getElementById("el3");
+
+window.addEventListener("scroll",whenScrolling())
+
+
+function whenScrolling()
+{
+	//code for scrolling animation for the three elements in the first seciton
+	//index page
+}
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(window).load(function(){
 	$(".util-pie").fadeOut("slow");
 });
+
+// when scrolling 
+
+var elementOne = document.getElementById("el1");
+var elementTwo = document.getElementById("el2");
+var elementThree = document.getElementById("el3");
+
+window.addEventListener("scroll",whenScrolling())
+
+
+function whenScrolling()
+{
+	//code for scrolling animation for the three elements in the first seciton
+	//index page
+}
+
+
 function showError(error)
 {
 	error.style.display ="block";

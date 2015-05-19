b4df9db88fae7fc8b6d43e6c0ac9d64042bf5ce2
hoenirvili <hoenirvili@gmail.com>
2015-05-19 15:27:29 +0300

    .gitignore is now working

--- src/application/config/local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('URL', 'http://localhost/EDeC/');
-define('ADMIN_URL', 'http://localhost/EDeC/admin/');
-define('DB_TYPE', 'oci');
-define('DB_HOST', '');
-define('DB_NAME', 'XE'); // Try ORCL if XE not working
-define('DB_USER', 'db_username'); // db username
-define('DB_PASS', 'db_pass'); // db pass

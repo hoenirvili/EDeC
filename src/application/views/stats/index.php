d9799648ed57be78ac49ebe78dd637adbe6fc7b7
hoenirvili <hoenirvili@gmail.com>
2015-05-21 17:18:43 +0300

    new bar stats created

--- src/application/views/stats/index.php
+++ src/application/views/stats/index.php
@@ -1,18 +1,54 @@
 
-<!--Most loved caracteristics -->
-<div class="container mlc box">
+<!--Most loved caracteristics-->
+<div class="container box">
    <div class="row">
-       <div class="col-md-12">
-         <canvas id="MostLovedCaract" width="400" height="400"></canvas>
+       <div class="col-md-4"></div>
+       <div class="col-md-4 mlc">
+            <div class="header-title-canvas">
+                <h1>Most hated caracteristics</h1>
+            </div>
+            <canvas id="MostLovedCaract" width="400" height="400"></canvas>
        </div>
-   </div>
-</div>
+       <div class="col-md-4"></div>
+   </div> <!--row-->
+</div> <!--container-->
 
 <!-- Most hated caracteristics -->
-<div class="container mhc box">
+<div class="container">
     <div class="row">
-        <div class="col-md-12">
-           <canvas id="MostHatedCaract"></canvas>
+        <div class="col-md-4"></div>
+
+        <div class="col-md-4 mhc">
+            <div class="header-title-canvas">
+                <h1>Most hated caracteristics</h1>
+            </div>
+           <canvas id="MostHatedCaract" width="400" height="400"></canvas>
         </div>
-    </div>
-</div>
+
+        <div class="col-md-4"></div>
+    </div><!--row-->
+</div><!--container-->
+
+
+<!--pop UP-->
+<!-- Loading animation svg -->
+<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"
+     class="util-pie">
+    <rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect>
+    <path d="M0 50A50 50 0 0 1 50 0L50 50L0 50" fill="#0073eb" opacity="0.5">
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="0.8s"
+                          repeatCount="indefinite"></animateTransform>
+    </path>
+    <path d="M50 0A50 50 0 0 1 100 50L50 50L50 0" fill="#00ff27" opacity="0.5">
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="1.6s"
+                          repeatCount="indefinite"></animateTransform>
+    </path>
+    <path d="M100 50A50 50 0 0 1 50 100L50 50L100 50" fill="#ff9400" opacity="0.5">
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="2.4s"
+                          repeatCount="indefinite"></animateTransform>
+    </path>
+    <path d="M50 100A50 50 0 0 1 0 50L50 50L50 100" fill="#ff3c00" opacity="0.5">
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="3.2s"
+                          repeatCount="indefinite"></animateTransform>
+    </path>
+</svg>
--- src/html/js/stats.js
+++ src/html/js/stats.js
@@ -1,37 +1,71 @@
 /**
- * config option of Chart
- * @type {CanvasRenderingContext2D}
+ *  Chart option config
+ *  @type {boolean}
  */
-//enable the responsive of charts
+// Make our chart responsive
 Chart.defaults.global.responsive = true;
 
 
-//Get the context of the canvas element that we want to select
-var ctx = document.getElementById("MostLovedCaract").getContext("2d"); // select our id
 
-// now e add data to our chart that we want to represent
-var data = {
-    labels  : ["rosii","castraveti","zahar","orez","paste"],
+var data1 = {
+    labels: ["Rosii", "Castraveti", "Zahar", "Orez", "Paste"],
     datasets: [
         {
             label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
             data: [110, 95, 80, 60, 43]
         },
-
         {
             label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86]
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 21]
         }
     ]
 };
-//we call call a method name of class Chart that we want to use
-var MostLovedCaracteristics = new Chart(ctx).PolarArea(data); // make a new object of chart
 
+// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#MostLovedCaract").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var mostLovedCaract =new Chart(ctx).Bar(data1);
+
+
+
+
+var data2 = {
+    labels: ["Gluten", "Brocoli", "Amidon", "Paine", "Pastarnac"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [180, 125, 88, 62, 48]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 21]
+        }
+    ]
+};
+
+var dtx = $("MostHatedCaract").get(0).getContext("2d");
+var mostHatedCaract = new Chart(dtx).Bar(data2);

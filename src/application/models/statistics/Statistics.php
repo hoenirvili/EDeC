f26947f6f948ee9f00728085bfdb1892c4e67c82
cornelius5241 <uucorneliu@gmail.com>
2015-05-27 17:50:13 +0300

    made php function to return array with hate statistics

--- src/application/models/statistics/Statistics.php
+++ src/application/models/statistics/Statistics.php
@@ -7,42 +7,54 @@
  */
 
 class Statistics {
+
     public static function getMostHated(){
 
         global $db;
-        $nr=5;
 
-        $sql = "
+      //  $db = oci_connect('edec', 'admin#522', 'localhost/XE');
+      //  if (!$db) {
+      //      $e = oci_error();
+      //      trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+      //  }
+
+        $stmt = "
                 BEGIN
-                  :return_cursor :=  EDEC_UTILS_PACKAGE.GET_HATE_STATS(:number);
+                    :return_cursor := EDEC_UTILS_PACKAGE.GET_HATE_STATS(:number_of_stats);
                 END;";
-        // Strip special characters to avoid ORA-06550 and PLS-00103 errors.
-        $sql = strip_special_characters($sql);
-        // Parse a query through the connection.
-        $query = oci_parse($db,$sql);
-        // Declare a return cursor for the connection.
-        $rc = oci_new_cursor($db);
-        // Bind PHP variables to the OCI output variable.
-        oci_bind_by_name($db,':return_cursor',$rc,-1,OCI_B_CURSOR);
-        // Bind PHP variables to the OCI input variable
-        oci_bind_by_name($db,':number',$nr);
+
+        $sql = oci_parse ($db,$stmt);// Parse a query through the connection.
+
+        $ret_crs = oci_new_cursor($db);// Declare a return cursor for the connection.
+
+        $number_of_stats = 10;
+        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
+        oci_bind_by_name($sql,'return_cursor',$ret_crs,-1,OCI_B_CURSOR);
 
         try {
-            // Execute the PL/SQL statement GET_HATE_STATS; reference cursor.
-            oci_execute($query);
-            oci_execute($rc);
+            oci_execute($sql);
+            oci_execute($ret_crs);
         } catch (PDOException $e) {
             db_exception($e);
             return false;
         }
+        $result=array();
+        while($row = oci_fetch_assoc($ret_crs)) {
+            $name=null;
+            $number=null;
+            foreach ($row as $v_name => $v_value){
+                if($v_name=='NAME')
+                    $name=$v_value;
+                if($v_name=='Number')
+                    $number=$v_value;
+            }
 
-       //$query->fetch(PDO::FETCH_OBJ);
+           $result[$name]=$number;
+        }
 
-        return $rc;
+        return $result  ;
 
     }
 
-    public static function getMostLoved(){
-        return null;
-    }
+
 }

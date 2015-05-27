b9565cd757e0a5dd87cb2c7d2e4668044383df7a
cornelius5241 <uucorneliu@gmail.com>
2015-05-27 18:22:42 +0300

    made php function to return array with love statistics

--- src/application/models/statistics/Statistics.php
+++ src/application/models/statistics/Statistics.php
@@ -56,5 +56,51 @@ class Statistics {
 
     }
 
-    
+    public static function getMostLoved(){
+
+        global $db;
+
+        //  $db = oci_connect('edec', 'admin#522', 'localhost/XE');
+        //  if (!$db) {
+        //      $e = oci_error();
+        //      trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+        //  }
+
+        $stmt = "
+                BEGIN
+                    :return_cursor := EDEC_UTILS_PACKAGE.GET_LOVE_STATS(:number_of_stats);
+                END;";
+
+        $sql = oci_parse ($db,$stmt);// Parse a query through the connection.
+
+        $ret_crs = oci_new_cursor($db);// Declare a return cursor for the connection.
+
+        $number_of_stats = 10;
+        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
+        oci_bind_by_name($sql,'return_cursor',$ret_crs,-1,OCI_B_CURSOR);
+
+        try {
+            oci_execute($sql);
+            oci_execute($ret_crs);
+        } catch (PDOException $e) {
+            db_exception($e);
+            return false;
+        }
+        $result=array();
+        while($row = oci_fetch_assoc($ret_crs)) {
+            $name=null;
+            $number=null;
+            foreach ($row as $v_name => $v_value){
+                if($v_name=='NAME')
+                    $name=$v_value;
+                if($v_name=='Number')
+                    $number=$v_value;
+            }
+
+            $result[$name]=$number;
+        }
+
+        return $result  ;
+
+    }
 }

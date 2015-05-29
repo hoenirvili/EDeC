f6fe9e3eaf847db5b2e95f59bdc5996f927a4ffd
cornelius5241 <uucorneliu@gmail.com>
2015-05-29 19:59:14 +0300

    resolved problem with sql functions product

--- sql/packages/4.edec_products_package.sql
+++ sql/packages/4.edec_products_package.sql
@@ -22,8 +22,8 @@ CREATE OR REPLACE PACKAGE edec_produse_package IS
   FUNCTION get_hate_stats(car_no IN NUMBER) RETURN SYS_REFCURSOR;
   FUNCTION get_love_stats(car_no IN NUMBER) RETURN SYS_REFCURSOR;
   
-  FUNCTION get_hate_stats_without_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR;
-  FUNCTION get_love_stats_without_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR;
+  FUNCTION get_hate_stats_with_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR;
+  FUNCTION get_love_stats_with_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR;
 END edec_produse_package;
 /
 
@@ -214,7 +214,7 @@ FUNCTION get_love_stats(car_no IN NUMBER) RETURN SYS_REFCURSOR IS
     RETURN love_cursor;
   END get_love_stats;
 
-FUNCTION get_hate_stats_without_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR IS
+FUNCTION get_hate_stats_with_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR IS
     hate_cursor SYS_REFCURSOR;--the result which will be returned
   
     v_carac_id caracteristica.id%TYPE:=-1;
@@ -227,7 +227,7 @@ FUNCTION get_hate_stats_without_car(car_no IN NUMBER,without_carac_name IN carac
                       produs prod 
                       JOIN caracteristici_produse car_prod ON prod.id=car_prod.produs_id
                       JOIN user_hates uh ON car_prod.caracteristica_id=uh.caracteristica_id
-                      WHERE car_prod.caracteristica_id not in (v_carac_id) AND ROWNUM <=car_no
+                      WHERE prod.id in (SELECT produs.id FROM produs JOIN caracteristici_produse car_prod ON produs.id=car_prod.PRODUS_ID WHERE car_prod.CARACTERISTICA_ID=v_carac_id ) AND ROWNUM <=car_no
                       GROUP BY name
                       ORDER BY Nr DESC; 
     
@@ -236,9 +236,9 @@ FUNCTION get_hate_stats_without_car(car_no IN NUMBER,without_carac_name IN carac
     WHEN NO_DATA_FOUND THEN
     raise_application_error(-20033,'INVALID FEATURE NAME GIVEN');
     
-  END get_hate_stats_without_car;
+  END get_hate_stats_with_car;
 
-FUNCTION get_love_stats_without_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR IS
+FUNCTION get_love_stats_with_car(car_no IN NUMBER,without_carac_name IN caracteristica.name%TYPE) RETURN SYS_REFCURSOR IS
     love_cursor SYS_REFCURSOR;--the result which will be returned
   
     v_carac_id caracteristica.id%TYPE:=-1;
@@ -251,16 +251,16 @@ FUNCTION get_love_stats_without_car(car_no IN NUMBER,without_carac_name IN carac
                       produs prod 
                       JOIN caracteristici_produse car_prod ON prod.id=car_prod.produs_id
                       JOIN user_loves ul ON car_prod.caracteristica_id=ul.caracteristica_id
-                      WHERE car_prod.caracteristica_id not in (v_carac_id) AND ROWNUM <=car_no
+                      WHERE prod.id in (SELECT produs.id FROM produs JOIN caracteristici_produse car_prod ON produs.id=car_prod.PRODUS_ID WHERE car_prod.CARACTERISTICA_ID=v_carac_id ) AND ROWNUM <=car_no
                       GROUP BY name
                       ORDER BY Nr DESC; 
     
-    RETURN hate_cursor;
+    RETURN love_cursor;
     EXCEPTION
     WHEN NO_DATA_FOUND THEN
     raise_application_error(-20033,'INVALID FEATURE NAME GIVEN');
     
-  END get_love_stats_without_car;
+  END get_love_stats_with_car;
 END EDEC_PRODUSE_PACKAGE;
 /
 

8025258d7e7612e55a7c05f719b73c6203c5f619
cornelius5241 <uucorneliu@gmail.com>
2015-05-29 20:41:28 +0300

    added function in php for products stats

--- src/application/models/statistics/Statistics.php
+++ src/application/models/statistics/Statistics.php
@@ -21,8 +21,8 @@ class Statistics {
 
         $ret_crs = oci_new_cursor($db->_dbh);// Declare a return cursor for the connection.
 
-        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
-        oci_bind_by_name($sql,':return_cursor',$ret_crs,-1,OCI_B_CURSOR);
+        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);//bind the value
+        oci_bind_by_name($sql,':return_cursor',$ret_crs,-1,OCI_B_CURSOR);//bind the cursor
 
         try {
             oci_execute($sql);
@@ -32,16 +32,15 @@ class Statistics {
             return false;
         }
         $result=array();
-        while($row = oci_fetch_assoc($ret_crs)) {
+        while($row = oci_fetch_assoc($ret_crs)) {//rows from cursor
             $name=null;
             $number=null;
-            foreach ($row as $v_name => $v_value){
+            foreach ($row as $v_name => $v_value){//columns from cursor
                 if($v_name=='NAME')
                     $name=$v_value;
                 if($v_name=='NR')
                     $number=$v_value;
             }
-
            $result[$name]=$number;
         }
 
@@ -222,6 +221,87 @@ class Statistics {
 
         oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
         oci_bind_by_name($sql,'return_cursor',$ret_crs,-1,OCI_B_CURSOR);
+
+        try {
+            oci_execute($sql);
+            oci_execute($ret_crs);
+        } catch (PDOException $e) {
+            db_exception($e);
+            return false;
+        }
+        $result=array();
+        while($row = oci_fetch_assoc($ret_crs)) {
+            $name=null;
+            $number=null;
+            foreach ($row as $v_name => $v_value){
+                if($v_name=='NAME')
+                    $name=$v_value;
+                if($v_name=='NR')
+                    $number=$v_value;
+            }
+
+            $result[$name]=$number;
+        }
+
+        return $result;
+    }
+
+
+    public static function getMostHatedProductsWFeature($number_of_stats,$feature){
+
+        global $db;
+        $stmt = "
+                BEGIN
+                    :return_cursor := EDEC_PRODUSE_PACKAGE.GET_HATE_STATS_WITH_CAR(:number_of_stats,:feature);
+                END;";
+
+        $sql = oci_parse ($db->_dbh,$stmt);// Parse a query through the connection.
+
+        $ret_crs = oci_new_cursor($db->_dbh);// Declare a return cursor for the connection.
+
+        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
+        oci_bind_by_name($sql,':feature',$feature);
+        oci_bind_by_name($sql,':return_cursor',$ret_crs,-1,OCI_B_CURSOR);
+
+        try {
+            oci_execute($sql);
+            oci_execute($ret_crs);
+        } catch (PDOException $e) {
+            db_exception($e);
+            return false;
+        }
+        $result=array();
+        while($row = oci_fetch_assoc($ret_crs)) {
+            $name=null;
+            $number=null;
+            foreach ($row as $v_name => $v_value){
+                if($v_name=='NAME')
+                    $name=$v_value;
+                if($v_name=='NR')
+                    $number=$v_value;
+            }
+
+            $result[$name]=$number;
+        }
+
+        return $result;
+    }
+
+    public static function getMostLovedProductsWFeature($number_of_stats,$feature){
+
+        global $db;
+        $stmt = "
+                BEGIN
+                    :return_cursor := EDEC_PRODUSE_PACKAGE.GET_LOVE_STATS_WITH_CAR(:number_of_stats,:feature);
+                END;";
+
+        $sql = oci_parse ($db->_dbh,$stmt);// Parse a query through the connection.
+
+        $ret_crs = oci_new_cursor($db->_dbh);// Declare a return cursor for the connection.
+
+        oci_bind_by_name($sql,':number_of_stats',$number_of_stats);
+        oci_bind_by_name($sql,':feature',$feature);
+        oci_bind_by_name($sql,':return_cursor',$ret_crs,-1,OCI_B_CURSOR);
 
         try {
             oci_execute($sql);

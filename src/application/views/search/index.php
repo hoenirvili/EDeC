2be2f2690ada90688364df1a94e595353c78af28
Calara Ionut <ionut.calara@gmail.com>
2015-05-31 16:27:35 +0300

    Wrapped up search and product

--- src/application/views/search/index.php
+++ src/application/views/search/index.php
@@ -1,20 +1,28 @@
 <body>
-    <div class="container">
+    <div class="container" id="products-search">
         <div class="row">
          <h2>&nbsp;</h2>
+
+            <form action="" method="GET">
              <div id="custom-search-input">
                 <div class="input-group col-md-12">
                     <!--search -->
-                    <input type="text" name="s" class="  search-query form-control" placeholder="Search" />
+                    <input type="text" name="s" class="  search-query form-control" value="<?php if(isset($_GET['s'])) echo $_GET['s']; ?>" placeholder="Search" />
                         <span class="input-group-btn">
                             <button class="btn btn-primary search-engine" type="submit">
                                 <span class=" glyphicon glyphicon-search"></span>
                             </button>
                         </span>
-                    </form>
                 </div><!--col md 12-->
             </div><!--custom search input-->
-            <?php Searcher::list_products(Searcher::fetch_best_match()); ?>
+
+            </form>
+            <?php
+            if(isset($_GET['s'])){
+                Searcher::list_products(Searcher::fetch_best_match($_GET['s']));
+            }else {
+                Searcher::list_products(Searcher::fetch_best_match());
+            }?>
 
         </div> <!--row-->
     </div> <!--container-->
--- src/html/js/product.js
+++ src/html/js/product.js
@@ -1,5 +1,74 @@
 $(document).ready(function() {
-    $('#myCarousel').carousel({
-        interval: 10000
-    })
+
+
+
+
+    if (jQuery('.add-ch').length) {
+        jQuery('.add-ch').on('click touchstart', function (e) {
+            e.preventDefault();
+            ch_id = jQuery(this).data('chid');
+            var t=jQuery(this);
+                    $.ajax({
+                        type: "POST",
+                        dataType: "json",
+                        url: '/ajax/add_to_loves/',
+                        data: {
+                            ch_id: ch_id
+                        },
+                        success: function (data) {
+                            if(data['status'])
+                            {
+
+                               var html='<a href="#" class=" btn btn-sm  btn-disabled button-menu-manage pull-right"><span class="glyphicon glyphicon-thumbs-up"></span></a>';
+                                var parent=t.parent()
+                                parent.find('a').remove();
+                                parent.append(html);
+                                alert('Succesfully added.');
+                            }
+                        }
+                    });
+
+
+        });
+    }
+
+    if (jQuery('.remove-ch').length) {
+        jQuery('.remove-ch').on('click touchstart', function (e) {
+            e.preventDefault();
+            ch_id = jQuery(this).data('chid');
+            var t=jQuery(this);
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: '/ajax/add_to_hates/',
+                data: {
+                    ch_id: ch_id
+                },
+                success: function (data) {
+                    if(data['status'])
+                    {
+
+                        var html='<a href="#" class=" btn btn-sm  btn-disabled button-menu-manage pull-right"><span class="glyphicon glyphicon-thumbs-down"></span></a>';
+                        var parent=t.parent()
+                        parent.find('a').remove();
+                        parent.append(html);
+                        alert('Succesfully added.');
+                    }
+                }
+            });
+
+
+        });
+    }
+
 });
+
+
+function disable_buttons()
+{
+    if (jQuery('.btn-disabled').length) {
+        jQuery('.btn-disabled').on('click touchstart', function (e) {
+            e.preventDefault();
+        });
+    };
+}

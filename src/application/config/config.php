43e454156ec4797baf198f01dc05e963883da0c9
Calara Ionut <ionut.calara@gmail.com>
2015-06-01 18:36:29 +0300

    Minor changes

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -11,4 +11,4 @@ define('APPLICATION_PATH', __DIR__.'/../../application/');
 define('CONTROLLER_PATH', APPLICATION_PATH.'controllers/');
 define('MODELS_PATH', APPLICATION_PATH.'models/');
 define('VIEWS_PATH', APPLICATION_PATH.'views/');
-define('PRODUCTION',true);
+define('PRODUCTION',false);
